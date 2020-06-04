4847390c77dbab71b912c96f3631d23012329895
ignacioalvaradot <ignacioalvaradot>
2020-06-04 18:07:42 +0000

    mejora  stats.sh

--- stats.sh
+++ stats.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+forma_de_uso(){
+	echo "Uso: $0 -d <archivo_datos> [h]"
+	exit 1
+}
 # La variable $# es equiv a argc
 if [ $# != 1 ]; then
-        echo "Uso: $0 <directorio busqueda>"
-        exit
+	forma_de_uso
 fi
-
+while getopts "d:h" opcion; do
+	case "$opcion" in
+		f)
+			dataIn=$OPTARG
+			;;
+		h)
+			forma_de_uso
+			;;
+		*)
+			forma_uso
+			;;
+	esac
+done
 searchDir=$1
 
